a1f783345dca410db51d36fc48d92d444eae94cd
Yeganeh <ymmarghi@gmail.com>
2020-09-15 22:44:45 -0700

    Update - data generator

--- augmenter/mnist_augmenter.py
+++ augmenter/mnist_augmenter.py
@@ -1,23 +1,24 @@
-import torch
+import os
 import torchvision.utils as vutils
 import matplotlib.pyplot as plt
-from module.udagan import Augmenter_mnist, Discriminator_mnist
+from module.udagan import Augmenter_mnist
 from module.dataloader import get_data
 from module.utils import *
 from matplotlib import gridspec
 
 fontsize = 6
-path = './results/mnist/'
-file = 'model_bs_64_dn_12_dz_10_lambda_0' #'model_bs_256_dn_12_dz_10'
+path = os.path.abspath(os.path.join(os.getcwd(), '..'))
+path += '/results/augmenter/mnist/'
+file = 'model_bs_128_dn_16_dz_10_lambda'
 load_file = path + file
 # Load the checkpoint file
 model = torch.load(load_file)
 
 # Set the device to run on: GPU or CPU
-device = torch.device("cuda:1" if(torch.cuda.is_available()) else "cpu")
+device = torch.device("cuda:0" if(torch.cuda.is_available()) else "cpu")
 # Get the 'parameters' dictionary from the loaded file
 parameters = model['parameters']
-n_samp = parameters['n_arm']
+n_samp = parameters['n_smp']
 
 # Initialise the network
 netA = Augmenter_mnist(noise_dim=parameters['num_n'],
@@ -27,9 +28,9 @@ netA = Augmenter_mnist(noise_dim=parameters['num_n'],
 netA.load_state_dict(model['netA'])
 # netD.load_state_dict(state_dict['netD'])
 
-dataloader = get_data(dataset='MNIST',
+dataloader, _ = get_data(dataset='MNIST',
                       batch_size=parameters['batch_size'],
-                      file_path='',
+                      file='',
                       n_feature=[])
 
 with torch.no_grad():
@@ -42,7 +43,7 @@ with torch.no_grad():
             augmented_img = []
             err = []
 
-            for arm in range(parameters['n_arm']):
+            for arm in range(parameters['n_smp']):
                 noise = torch.randn(b_size, parameters['num_n'], device=device)
                 noise += 0.1 * torch.sign(noise)
                 # zeros = torch.zeros(b_size, parameters['num_n'], 1, 1,
@@ -52,7 +53,7 @@ with torch.no_grad():
 
             ns = 15
             fig = plt.figure(figsize=(4, 3))
-            gs = gridspec.GridSpec(1, 2, width_ratios=[1, parameters['n_arm']])
+            gs = gridspec.GridSpec(1, 2, width_ratios=[1, parameters['n_smp']])
             ax = plt.subplot(gs[0])
             ax.spines['right'].set_visible(False)
             ax.spines['top'].set_visible(False)
@@ -67,7 +68,7 @@ with torch.no_grad():
             concat_img = torch.zeros((1, real_data.size(2),
                                       real_data.size(-1)))
             for n_smp in range(ns):
-                for arm in range(parameters['n_arm']):
+                for arm in range(parameters['n_smp']):
                     concat_img = torch.cat((concat_img, augmented_img[arm][
                                               n_smp, :, :, :]), dim=0)
             concat_img = concat_img.unsqueeze(1)
@@ -81,7 +82,7 @@ with torch.no_grad():
             ax.set_ylabel('Augmented Data')
             plt.imshow(
                 np.transpose(vutils.make_grid(concat_img[1:, :, :, :],
-                        nrow=parameters['n_arm'], padding=2, normalize=True),
+                        nrow=parameters['n_smp'], padding=2, normalize=True),
                              (1, 2, 0)), aspect='auto')
             plt.subplots_adjust(wspace=0.1)
             plt.tight_layout()
